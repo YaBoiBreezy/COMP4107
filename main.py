92e0c9f114ec1c00c561fc60ab229d70f95bdb24
Alex Breeze <91281967+YaBoiBreezy@users.noreply.github.com>
2023-03-06 17:48:27 -0500

    created NNand trained, not tested yet

--- main.py
+++ main.py
@@ -11,19 +11,53 @@ from PIL import Image, ImageDraw
 import cv2
 import glob
 import random
+import talos
+
+def custom_loss(y,yhat): #using lanbda function to have access to yhat while iterating over y
+ c1=y #loss is minimum of sum of squares of manhattan distances between pair of points yhat and all pairs of points in y
+ c2=yhat
+ distances=map_fn(lambda c1: tf.add(tf.square(tf.add(tf.abs(tf.subtract(c1[0],c2[0])),tf.abs(tf.subtract(c1[1],c2[1])))),tf.square(tf.add(tf.abs(tf.subtract(c1[2],c2[2])),tf.abs(tf.subtract(c1[3],c2[3])))))
+ return tf.minimum(distances)
 
 # A function that implements a keras model with the sequential API
-def sequential_model():
+def createModel(xTrain, yTrain, xVal, yVal, params):
+ model = Sequential()
+ model.add(layers.Conv2D(filters=params['filter1'], kernel_size=(3,3,3), activation=params['convActivation'], input_shape=(1024, 1024, 3)))
+ model.add(layers.MaxPooling2D(params['pool1']))
+ model.add(layers.Conv2D(filters=params['filter2'], kernel_size=params['kernel2'], activation=params['convActivation']))
+ model.add(layers.MaxPooling2D(params['pool2']))
+ model.add(layers.Conv2D(filters=params['filter3'], kernel_size=params['kernel3'], activation=params['convActivation']))
+ model.add(layers.MaxPooling2D(params['pool3']))
+ model.add(layers.Conv2D(filters=params['filter4'], kernel_size=params['kernel4'], activation=params['convActivation']))
+ model.add(layers.MaxPooling2D(params['pool4']))
+ model.add(layers.Conv2D(filters=params['filter5'], kernel_size=params['kernel5'], activation=params['convActivation']))
+ model.add(layers.MaxPooling2D(params['pool5']))
+ model.add(layers.Flatten())
+ model.add(layers.Dense(params['neuron1'], activation=params['NNActivation']))
+ model.add(layers.Dense(params['neuron2'], activation=params['NNActivation']))
+ model.add(layers.Dense(params['neuron3'], activation=params['NNActivation']))
+ model.add(layers.Dense(params['neuron4'], activation=params['NNActivation']))
+ model.add(layers.Dense(params['neuron5'], activation=params['NNActivation']))
+ model.add(layers.Dense(4, activation='softplus'))
+
+ model.compile(loss=custom_loss, optimizer='adam') #, metrics=['accuracy']
+ out = model.fit(x=xTrain, y=yTrain, validation_data=[xVal, yVal], epochs=100, batch_size=params['batch_size'], verbose=0)
+
+return out,model
+
   model=keras.Sequential()
   model.add(keras.layers.Dense(12, input_shape=(4,), activation="relu"))
   model.add(keras.layers.Dense(6, activation="relu"))
-  model.add(keras.layers.Dense(3, activation="softmax"))
+  model.add(keras.layers.Dense(3, activation="abs"))
   print(model.summary())
   optimizer=tf.keras.optimizers.SGD() #learning_rate=0.001
   model.compile(optimizer=optimizer, loss="cross_entropy")
 
   # A keras model
   return model
+
+def trainModel(x,y):
+ return model,loss
 
 def drawRectangles(image,chords):
  image2=ImageDraw.Draw(image)
@@ -58,6 +92,10 @@ def generateData(backgrounds,sprites,numInstances,minSpriteCount,maxSpriteCount,
   data.append(compound.getdata())
   compound=drawRectangles(compound,y[-1])
  data=np.array(data).reshape((numInstances,1024,1024,3))
+ for x in range(len(y)):
+  for z in range(maxSpriteCount-len(y[x])):
+   y[x].append([-1024,-1024,-1024,-1024]) #filling out y, make sure NN can only output positive coordinates.
+ y=np.array(y)
  return data, y
 
 #takes the locations of 2 folders of images, returns 2 numpy arrays of those images
@@ -69,11 +107,35 @@ def readData():
  for x in range(1,101):
   sprites.append(Image.open("./sprites/sprite"+str(x)+".png"))
  return backgrounds, sprites
-b,s=readData()
-data,y=generateData(b,s,10,2,4,1,1,1)
-#print(data)
-print(len(data))
-print(len(data[0]))
-print(len(data[0][0]))
-print(len(data[0][0][0]))
-print(y)
+
+def main():
+ backgrounds,sprites=readData()
+ b1, b2 = train_test_split(backgrounds, test_size=0.25) #split backgrounds such that |b1|=60, |b2|=20
+ s1, s2 = train_test_split(sprites, test_size=0.2) #split sprites such that |s1|=75, |s2|=25
+ trainData,trainY=generateData(b1,s1,1000,2,4,0,1,0)
+ valData,valY=generateData(b2,s2,200,2,4,0,1,0)
+
+ p = {
+    'filter1': [8, 16, 32, 64],
+    'filter2': [8, 16, 32, 64],
+    'filter3': [8, 16, 32, 64],
+    'filter4': [8, 16, 32, 64],
+    'filter5': [8, 16, 32, 64],
+    'pool1': [2, 3, 4, 5],
+    'pool2': [2, 3, 4, 5],
+    'pool3': [2, 3, 4, 5],
+    'pool4': [2, 3, 4, 5],
+    'pool5': [2, 3, 4, 5],
+    'neuron1': [512, 1024, 2048, 4096, 8192, 16384],
+    'neuron2': [512, 1024, 2048, 4096, 8192, 16384],
+    'neuron3': [512, 1024, 2048, 4096, 8192, 16384],
+    'neuron4': [512, 1024, 2048, 4096, 8192, 16384],
+    'neuron5': [512, 1024, 2048, 4096, 8192, 16384],
+    'convActivation': ['sigmoid', 'elu', 'linear', 'softmax'],
+    'NNActivation': ['sigmoid', 'elu', 'linear', 'softmax'],
+    'batch_size': (10,100,10)}  #(min,max,steps)
+
+ t = talos.Scan(x=trainData, y=trainY, x_val=valData, y_val=valY, params=p, model=createModel, 
+                experiment_name='duplicateFinder', round_limit=5) #use performance_target maybe
+ print(t.best_model(metric='f1score', asc=True)) #metric must be one used in experiment, asc=True bc want to minimize
+ print(t.details)
