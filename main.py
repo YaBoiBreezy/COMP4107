9b828ba3805090f89733da5ad23b4133758882e5
Alex Breeze <91281967+YaBoiBreezy@users.noreply.github.com>
2023-03-07 10:08:38 -0500

    stuff

--- main.py
+++ main.py
@@ -16,11 +16,13 @@ import talos
 def custom_loss(y,yhat): #using lanbda function to have access to yhat while iterating over y
  c1=y #loss is minimum of sum of squares of manhattan distances between pair of points yhat and all pairs of points in y
  c2=yhat
- distances=map_fn(lambda c1: tf.add(tf.square(tf.add(tf.abs(tf.subtract(c1[0],c2[0])),tf.abs(tf.subtract(c1[1],c2[1])))),tf.square(tf.add(tf.abs(tf.subtract(c1[2],c2[2])),tf.abs(tf.subtract(c1[3],c2[3])))))
+ distances=map_fn(lambda c1: tf.add(tf.square(tf.add(tf.abs(tf.subtract(c1[0],c2[0])),tf.abs(tf.subtract(c1[1],c2[1])))),tf.square(tf.add(tf.abs(tf.subtract(c1[2],c2[2])),tf.abs(tf.subtract(c1[3],c2[3]))))))
  return tf.minimum(distances)
 
 # A function that implements a keras model with the sequential API
 def createModel(xTrain, yTrain, xVal, yVal, params):
+ print("creating model:")
+ print(params)
  model = Sequential()
  model.add(layers.Conv2D(filters=params['filter1'], kernel_size=(3,3,3), activation=params['convActivation'], input_shape=(1024, 1024, 3)))
  model.add(layers.MaxPooling2D(params['pool1']))
@@ -42,19 +44,9 @@ def createModel(xTrain, yTrain, xVal, yVal, params):
 
  model.compile(loss=custom_loss, optimizer='adam') #, metrics=['accuracy']
  out = model.fit(x=xTrain, y=yTrain, validation_data=[xVal, yVal], epochs=100, batch_size=params['batch_size'], verbose=0)
-
-return out,model
-
-  model=keras.Sequential()
-  model.add(keras.layers.Dense(12, input_shape=(4,), activation="relu"))
-  model.add(keras.layers.Dense(6, activation="relu"))
-  model.add(keras.layers.Dense(3, activation="abs"))
-  print(model.summary())
-  optimizer=tf.keras.optimizers.SGD() #learning_rate=0.001
-  model.compile(optimizer=optimizer, loss="cross_entropy")
-
-  # A keras model
-  return model
+ print(out)
+ print(model)
+ return out,model
 
 def trainModel(x,y):
  return model,loss
@@ -72,10 +64,9 @@ def generateData(backgrounds,sprites,numInstances,minSpriteCount,maxSpriteCount,
  for _ in range(numInstances):
   y.append([])
   sprite=random.choice(sprites)
-  compound=random.choice(backgrounds).copy().resize((1024,1024))
+  compound=random.choice(backgrounds).copy()
   for _ in range(random.randint(minSpriteCount,maxSpriteCount)):
    tempSprite=sprite.copy()
-   tempSprite.thumbnail((512,512),PIL.Image.LANCZOS)
    if rotation:
     tempSprite=tempSprite.rotate(random.randint(0,360), expand=1) #true makes it resize to fit new image. Uses nearest neighbor to keep pixel colors
    if sizing:
@@ -94,7 +85,7 @@ def generateData(backgrounds,sprites,numInstances,minSpriteCount,maxSpriteCount,
  data=np.array(data).reshape((numInstances,1024,1024,3))
  for x in range(len(y)):
   for z in range(maxSpriteCount-len(y[x])):
-   y[x].append([-1024,-1024,-1024,-1024]) #filling out y, make sure NN can only output positive coordinates.
+   y[x].append([-1024,-1024,-1024,-1024]) #filling out y, NN can only output positive coordinates due to a()=softplus
  y=np.array(y)
  return data, y
 
@@ -102,19 +93,30 @@ def generateData(backgrounds,sprites,numInstances,minSpriteCount,maxSpriteCount,
 def readData():
  backgrounds = []
  for x in range(1,80):
-  backgrounds.append(Image.open("./backgrounds/background"+str(x)+".jpg"))
+  backgrounds.append(Image.open("./backgrounds/background"+str(x)+".jpg").resize((1024,1024)))
  sprites = []
- for x in range(1,101):
-  sprites.append(Image.open("./sprites/sprite"+str(x)+".png"))
+ for x in range(100):
+  img=Image.open("./sprites/sprite"+str(x)+".png")
+  img.thumbnail((512,512),PIL.Image.LANCZOS)
+  sprites.append(img)
  return backgrounds, sprites
 
 def main():
+ trainSize=80
+ testSize=20
  backgrounds,sprites=readData()
+ print("read data")
  b1, b2 = train_test_split(backgrounds, test_size=0.25) #split backgrounds such that |b1|=60, |b2|=20
  s1, s2 = train_test_split(sprites, test_size=0.2) #split sprites such that |s1|=75, |s2|=25
- trainData,trainY=generateData(b1,s1,1000,2,4,0,1,0)
- valData,valY=generateData(b2,s2,200,2,4,0,1,0)
+ trainData,trainY=generateData(b1,s1,trainSize,2,4,0,1,0)
+ valData,valY=generateData(b2,s2,testSize,2,4,0,1,0)
+ print("generated data")
+ print(trainData.shape)
+ print(valData.shape)
+ print(trainY.shape)
+ print(valY.shape)
 
+ #hyperparameters of neural network to optimize
  p = {
     'filter1': [8, 16, 32, 64],
     'filter2': [8, 16, 32, 64],
@@ -133,9 +135,10 @@ def main():
     'neuron5': [512, 1024, 2048, 4096, 8192, 16384],
     'convActivation': ['sigmoid', 'elu', 'linear', 'softmax'],
     'NNActivation': ['sigmoid', 'elu', 'linear', 'softmax'],
-    'batch_size': (10,100,10)}  #(min,max,steps)
+    'batch_size': (10,trainSize,10)}  #(min,max,steps)
 
- t = talos.Scan(x=trainData, y=trainY, x_val=valData, y_val=valY, params=p, model=createModel, 
-                experiment_name='duplicateFinder', round_limit=5) #use performance_target maybe
+ t = talos.Scan(x=trainData, y=trainY, x_val=valData, y_val=valY, params=p, model=createModel, experiment_name='duplicateFinder', round_limit=5) #use performance_target maybe
  print(t.best_model(metric='f1score', asc=True)) #metric must be one used in experiment, asc=True bc want to minimize
  print(t.details)
+
+main()
